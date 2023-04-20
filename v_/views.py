e485b1e21aa987fb9dbf0f2c3c8fb241cc0a9686
lanpigwen <783815828@qq.com>
2023-04-20 20:54:45 +0800

    111

--- v_/views.py
+++ v_/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render, redirect,HttpResponse
 import json
 
+# 验证webhook
+import hashlib
+import hmac
+
+# secret token git webhook
+secret_key="123456" 
+
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+    
+    Raise and return 403 if not authorized.
+    
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        raise HttpResponse('x-hub-signature-256 header is missing!', status=403)
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise HttpResponse("Request signatures didn't match!", status=403)
+    return 1
+
+
 #以下两行的作用是让结果唯一：
 from langdetect import DetectorFactory
 DetectorFactory.seed = 0
@@ -19,13 +45,16 @@ def vrHouse(request):
 
 def autopull(request):
     if request.method=='POST':
-
         hookdata=request.body.decode('utf-8')
         data=json.loads(hookdata)
-        # print(data.headers('X-Hub-Signature-256'))
-        print('/n----------------------收到webhook------------------------------/n')
-        print(request.headers.get('X-Hub-Signature-256'))
-        ans={"from":"post66666"}
-        return HttpResponse(json.dumps(ans))
+        print('\n----------------------收到webhook------------------------------\n')
+        sig_header=request.headers.get('X-Hub-Signature-256')
+        if verify_signature(request.body,secret_key,sig_header):
+            print('\n----------------signatures match-------------------------\n')
+            ans={"from":"post"}
+            return HttpResponse(json.dumps(ans))
+        
+        ans={"from":"post fail"}
+        return HttpResponse(json.dumps(ans))    
     
-    
+
